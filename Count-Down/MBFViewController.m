2e74531702fe6e55284dfd2faeab66352a5c1b0a
Ryan McCawley <ryan.mccawley@me.com>
2014-07-09 14:51:07 -0400

    FIRST COMMIT

--- Count-Down/MBFViewController.m
+++ Count-Down/MBFViewController.m
@@ -18,12 +18,56 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //QUESTION #1//
+    //[self printAllWholeNumbersBelow:5];
+    //QUESTION #2//
+    //[self printAllWholeNumbersBetweenFirstInteger:20 andSecondInteger:30];
+    //QUESTION #3//
+    int answerForFactorial = [self factorialOfInteger:9];
+    NSLog(@"%i", answerForFactorial);
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//QUESTION 1
+-(void)printAllWholeNumbersBelow:(int)number
+{
+    if (number > 0) {
+        for (int i = number; i > 0; i --){
+            NSLog(@"%i", i);
+        }
+    }
+}
+
+-(void)printAllWholeNumbersBetweenFirstInteger:(int)firstNumber andSecondInteger:(int)secondNumber
+{
+    if (firstNumber == secondNumber){
+        NSLog(@"Numbers are equal");
+    }
+    else if (firstNumber > secondNumber){
+        for (int i = firstNumber; i >= secondNumber ; i --) {
+            NSLog(@"%i", i);
+        }
+    }
+    else{
+        for (int i = secondNumber; i >= firstNumber; i --){
+            NSLog(@"%i", i);
+        }
+    }
+}
+
+-(int)factorialOfInteger:(int)number
+{
+    int answer = number;
+    for (int i = number; i >1; i --){
+        answer = answer * (i -1);
+        NSLog(@"i: %i answer: %i", i, answer);
+    }
+    return answer;
 }
 
 @end
